7a9cb028d89871c674d635715af69d43830cf579
min <7kyeongmin@gmail.com>
2018-12-04 12:16:15 +0900

    chat Network

---
+++ Assets/Scripts/NetworkChatManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+using UnityEngine.UI;
+
+public class NetworkChatManager : MonoBehaviour
+{
+    SocketIOComponent socket;
+
+    public InputField messageInputField;
+    public Text messageText;
+    string nickname;
+
+    void Start()
+    {
+        GameObject io = GameObject.Find("SocketIO");
+        socket = io.GetComponent<SocketIOComponent>();
+
+        nickname = "hong";  //TODO: 서버 닉네임 가져오기!
+        socket.On("chat", UpdateMessage);
+    }
+    public void UpdateMessage(SocketIOEvent e )
+    {
+        //e에는 서버가 보내느 msg정보가 들어있다?
+    //    var date = e.data;
+      //  Debug.Log("Recived message...");
+
+        string nick = e.data.GetField("nick").str;
+        string msg = e.data.GetField("msg").str;
+
+        messageText.text += string.Format(" {0} : {1}\n", nick, msg);
+    }
+
+    public void Send()
+    {
+        //자신의 메시지 화면에 표시
+        string message = messageInputField.text;
+        messageText.text += string.Format(" {0} : {1}\n", nickname, message);
+        messageInputField.text = "";
+
+        //자신이 입력한 메시지 서버에 전송
+        JSONObject obj = new JSONObject();
+        obj.AddField("nick", nickname);
+        obj.AddField("msg", message);
+        socket.Emit("message", obj);
+       
+    }
+}
---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+using UnityEngine.UI;
+
+
+
+public class NetworkManager : MonoBehaviour {
+
+    public Text resultText;
+    SocketIOComponent socket;
+	
+	void Start () {
+        
+
+        GameObject io = GameObject.Find("SocketIO");
+        socket = io.GetComponent<SocketIOComponent>();
+
+        socket.On("hello", Hello); //메세지 등록
+    }
+
+    void Hello(SocketIOEvent e )
+    {
+        Debug.Log("Hello");
+        resultText.text = "Hello";
+    }
+    public void Hi()
+    {
+        socket.Emit("hi");
+    }
+}
