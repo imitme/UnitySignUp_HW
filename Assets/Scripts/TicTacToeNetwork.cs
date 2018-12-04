eb21f5f25dad8da10f6c2c0fb4751ce590917e96
min <7kyeongmin@gmail.com>
2018-12-04 12:56:33 +0900

    tictactoe first setting

---
+++ Assets/Scripts/TicTacToeNetwork.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class TicTacToeNetwork : MonoBehaviour {
+
+    SocketIOComponent socket;
+
+	void Start () {
+        GameObject so = GameObject.Find("SocketIO");
+        socket = so.GetComponent<SocketIOComponent>();
+
+        socket.On("joinRoom", JoinRoom);
+        socket.On("createRoom", CreateRoom);
+    }
+
+    void JoinRoom(SocketIOEvent e)
+    {
+        Debug.Log("Join Room.");
+    }
+    void CreateRoom(SocketIOEvent e)
+    {
+        Debug.Log("Create Room.");
+    }
+
+}
